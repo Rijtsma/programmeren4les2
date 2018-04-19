5bf5619ae3e0d609eaeafefb15e661fe89f6e1e1
LAPTOP-C4B9JAEN\ricki <rijtsma@avans.nl>
2018-04-19 15:52:23 +0200

    Port aangespast

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 
 var http = require("http")
 
+const port = process.env.PORT || 3000;
+
 function onRequest(request, response){
     console.log("Er was een request.")
     response.writeHead(200,{"Content-Type": "application/json"})
@@ -15,6 +17,6 @@ function onRequest(request, response){
     response.end(result);
 }
 
-http.createServer(onRequest).listen(3000)
+http.createServer(onRequest).listen(port || 3000)
 
-console.log("De server luistert op port 3000")
+console.log("De server luistert op port " + port)
