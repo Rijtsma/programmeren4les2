79e26090464c6f7cf0df19f856a40d428685993c
LAPTOP-C4B9JAEN\ricki <rijtsma@avans.nl>
2018-04-19 15:27:45 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require("http")
+
+function onRequest(request, response){
+    console.log("Er was een request.")
+    response.writeHead(200,{"Content-Type": "application/json"})
+    let result = JSON.stringify({
+        "firstname": "Rick",
+        "lastname:": "IJtsma"
+    })
+    
+    response.end(result);
+}
+
+http.createServer(onRequest).listen(3000)
+
+console.log("De server luistert op port 3000")
